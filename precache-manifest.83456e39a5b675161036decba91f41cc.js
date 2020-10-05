7d1232f9c69515b5f0f7bd3cdd93250601e46f4f
ShirleyTZ <553728720@qq.com>
2020-10-06 01:45:06 +0800

    Updates

---
+++ precache-manifest.83456e39a5b675161036decba91f41cc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df3f768c93a2c7f003fce786113a2120",
+    "url": "/xiaomi-mobile/index.html"
+  },
+  {
+    "revision": "222dbd0729b69124f9c8",
+    "url": "/xiaomi-mobile/static/css/main.46d7529b.chunk.css"
+  },
+  {
+    "revision": "16c19087b6fc64276ec9",
+    "url": "/xiaomi-mobile/static/js/2.edcb0e23.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/xiaomi-mobile/static/js/2.edcb0e23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "222dbd0729b69124f9c8",
+    "url": "/xiaomi-mobile/static/js/main.80bd0a23.chunk.js"
+  },
+  {
+    "revision": "d56c685aa0628c33526f",
+    "url": "/xiaomi-mobile/static/js/runtime-main.3c9889b0.js"
+  },
+  {
+    "revision": "63c8e198511e3b36f6106433126ac4e8",
+    "url": "/xiaomi-mobile/static/media/bg.63c8e198.png"
+  },
+  {
+    "revision": "52a68e3664ee9af35f66586540d9b610",
+    "url": "/xiaomi-mobile/static/media/icon-star-selected.52a68e36.png"
+  }
+]);
